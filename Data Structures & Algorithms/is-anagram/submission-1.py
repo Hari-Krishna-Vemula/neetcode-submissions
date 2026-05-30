e51ae8cad41d81dc66c11af3de946f895395ccb7
Hari-Krishna-Vemula <cyber.technptel2025@gmail.com>
2026-05-30 19:02:03 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)==len(t):
+            l1=list(s)
+            l2=list(t)
+            for ch in s:
+                ch=l1[0]
+                if ch not in l2:
+                    return False
+                elif ch in l2:
+                    l1.remove(ch)
+                    l2.remove(ch)
+                
+            if len(l1)==0:
+                return True
+        else:
+            return False
